57d3a0d691860fdce9daf77c2787cdb5cb0a3c4e
Jakub <czakon.jakub@gmail.com>
2020-08-01 14:35:11 +0200

    Update train.py

--- train.py
+++ train.py
@@ -5,5 +5,5 @@ neptune.init(project_qualified_name='shared/github-actions', api_token='ANONYMOU
 neptune.create_experiment(params={'lr': 0.3, 'epoch_nr': 11})
 # neptune.append_tag('baseline')
 
-neptune.log_metric('accuracy', 0.76)
+neptune.log_metric('accuracy', 0.99)
 neptune.log_metric('f1_score', 0.69)
