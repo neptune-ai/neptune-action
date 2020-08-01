6cac4ad94e4718a87261c9df518fdddac690bbfe
Jakub <czakon.jakub@gmail.com>
2020-08-01 15:06:02 +0200

    Update train.py

--- train.py
+++ train.py
@@ -6,4 +6,4 @@ neptune.create_experiment(params={'lr': 0.3, 'epoch_nr': 888})
 # neptune.append_tag('baseline')
 
 neptune.log_metric('accuracy', 0.99)
-neptune.log_metric('f1_score', 0.69)
+neptune.log_metric('f1_score', 0.22)
