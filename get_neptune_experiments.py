be3b6d28498c8852f3ed88b065e9986e2b08325f
jakubczakon <czakon.jakub@gmail.com>
2020-08-02 12:28:27 +0200

    testing getting secrets

--- get_neptune_experiments.py
+++ get_neptune_experiments.py
@@ -1,24 +1,28 @@
+import sys
+
 import neptune
 
-PROJECT_NAME = 'shared/github-actions'
-API_TOKEN = 'ANONYMOUS'
+PROJECT_NAME = sys.getenv('NEPTUNE_PROJECT_NAME')
+API_TOKEN = sys.getenv('NEPTUNE_API_TOKEN')
 
-def get_experiment_data(ids):
+BRANCHES = ['master', 'develop']
+EXPERIMENT_IDS = ['GIT-3', 'GIT-6']
+
+def get_experiment_data():
     project = neptune.init(project_qualified_name=PROJECT_NAME, api_token=API_TOKEN)
-    
-    return project.get_leaderboard(id=ids)
-    
+
+    return project.get_leaderboard(id=EXPERIMENT_IDS)
+
 
 def find_diff(df):
-    
     selected_cols, cleaned_cols = [], []
     for col in df.columns:
         for name in ['channel_', 'parameter_', 'property_']:
             if name in col and not any(excluded_name in col for excluded_name in ['stderr', 'stdout']):
                 selected_cols.append(col)
-                cleaned_cols.append(col.replace(name,''))
+                cleaned_cols.append(col.replace(name, ''))
     df = df[['id'] + selected_cols]
-        
+
     different_cols = []
     for col in df.columns:
         vals = df[col].values
@@ -28,28 +32,27 @@ def find_diff(df):
     return df[different_cols]
 
 
-
 def format_data(df):
-    
     data = df.to_dict()
-    
-    cleaned_data = {'metrics':{},
-                    'parameters':{},
-                    'properties':{},
-                    'branches':['master', 'develop']
-                   }
-    for k,v in data.items():
-        if k=='id':
-            cleaned_data[k] = [v[0],v[1]] 
-            
+
+    cleaned_data = {'metrics': {},
+                    'parameters': {},
+                    'properties': {},
+                    'branches': BRANCHES
+                    }
+    for k, v in data.items():
+        if k == 'id':
+            cleaned_data[k] = [v[0], v[1]]
+
         if 'channel_' in k:
-            cleaned_data['metrics'][k.replace('channel_','')] = [v[0],v[1]]
+            cleaned_data['metrics'][k.replace('channel_', '')] = [v[0], v[1]]
         if 'parameter_' in k:
-            cleaned_data['parameters'][k.replace('parameter_','')] = [v[0],v[1]]
+            cleaned_data['parameters'][k.replace('parameter_', '')] = [v[0], v[1]]
         if 'property_' in k:
-            cleaned_data['properties'][k.replace('property_','')] = [v[0],v[1]]
-            
+            cleaned_data['properties'][k.replace('property_', '')] = [v[0], v[1]]
+
     return cleaned_data
+
 
 def create_table(data):
     table = []
@@ -59,12 +62,13 @@ def create_table(data):
     for branch in data['branches']:
         text = "<td><b>{}</b></td>".format(branch)
         table.append(text)
-    
+
     # experiment links and id
     table.append("<tr><td>Neptune Experiment</td>")
     user, project = PROJECT_NAME.split('/')
     for exp_id in data['id']:
-        text = """<td><a href="https://ui.neptune.ai/o/{0}/org/{1}/e/{2}"><b>{2}</b></a></td>""".format(user, project, exp_id)
+        text = """<td><a href="https://ui.neptune.ai/o/{0}/org/{1}/e/{2}"><b>{2}</b></a></td>""".format(user, project,
+                                                                                                        exp_id)
         table.append(text)
     table.append("</tr>")
 
@@ -82,7 +86,7 @@ def create_table(data):
             table.append("</tr>")
 
         table.append("</tr>")
-    
+
     # parameters
     if data['parameters']:
         table.append("""<tr>
@@ -97,9 +101,9 @@ def create_table(data):
             table.append("</tr>")
 
         table.append("</tr>")
-    
+
     # properties
-    
+
     if data['properties']:
         table.append("""<tr>
             <th colspan=3, class="section-type", style="text-align:left;background-color:lightblue;">
@@ -113,19 +117,19 @@ def create_table(data):
             table.append("</tr>")
 
         table.append("</tr>")
-    
+
     table.append("</tr></table>")
-    
+
     table_text = "".join(table)
-        
+
     return table_text
 
-if __name__ == "__main__":
 
-    df = get_experiment_data(['GIT-3', 'GIT-6'])
+if __name__ == "__main__":
+    df = get_experiment_data()
     df_diff = find_diff(df)
     data = format_data(df_diff)
     table_text = create_table(data)
-    
+
     with open("comparison_table.md", "w+") as f:
         f.write(table_text)
