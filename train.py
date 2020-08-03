116ca580e5c5b1878a562366fcee78d8e4db5c9d
jakubczakon <czakon.jakub@gmail.com>
2020-08-03 10:28:49 +0200

    aaa

--- train.py
+++ train.py
@@ -1,13 +1,11 @@
-# import neptune
-#
-# neptune.init(project_qualified_name='shared/github-actions', api_token='ANONYMOUS')
-#
-# neptune.create_experiment(params={'lr': 0.3, 'epoch_nr': 40})
-#
-# neptune.log_metric('accuracy', 0.81)
-# neptune.log_metric('f1_score', 0.66)
+import neptune
 
-weird_text="bla"
+neptune.init(project_qualified_name='shared/github-actions', api_token='ANONYMOUS')
+
+neptune.create_experiment(params={'lr': 0.3, 'epoch_nr': 40})
+
+neptune.log_metric('accuracy', 0.81)
+neptune.log_metric('f1_score', 0.66)
 
 import os
-os.environ["NEPTUNE_EXPERIMENT_ID"] = "GIT-3" #neptune.get_experiment().id
+os.environ["NEPTUNE_EXPERIMENT_ID"] = neptune.get_experiment().id
