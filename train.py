4b05f3a9a9c570d9327f1037a3257857aa88e4b2
Jakub <czakon.jakub@gmail.com>
2020-08-03 17:07:20 +0200

    Update train.py

--- train.py
+++ train.py
@@ -15,11 +15,11 @@ API_TOKEN = os.getenv('NEPTUNE_API_TOKEN')
 PARAMS = {'boosting_type': 'gbdt',
           'objective': 'multiclass',
           'num_class': 3,
-          'num_leaves': 8,
+          'num_leaves': 14,
           'learning_rate': 0.01,
-          'feature_fraction': 0.9
+          'feature_fraction': 0.7
           }
-NUM_BOOSTING_ROUNDS = 10
+NUM_BOOSTING_ROUNDS = 20
 
 neptune.init(api_token=API_TOKEN, project_qualified_name=PROJECT_NAME)
 neptune.create_experiment('lightGBM-on-wine', params={**PARAMS, 'num_boosting_round': NUM_BOOSTING_ROUNDS})
