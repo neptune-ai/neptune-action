4d83956e804fd31c63288b5bad73cf69b71aad2e
Jakub <czakon.jakub@gmail.com>
2020-08-03 10:41:01 +0200

    Update train.py

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ neptune.init(project_qualified_name='shared/github-actions', api_token='ANONYMOU
 neptune.create_experiment(params={'lr': 0.3, 'epoch_nr': 40})
 
 neptune.log_metric('accuracy', 0.667)
-neptune.log_metric('f1_score', 0.66)
+neptune.log_metric('f1_score', 0.3312)
 
 import os
 os.environ["NEPTUNE_EXPERIMENT_ID"] = neptune.get_experiment().id
