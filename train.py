f184263d9c73574ac83c82ba26f818d903117e6b
Jakub <czakon.jakub@gmail.com>
2020-08-03 10:23:05 +0200

    Update train.py

--- train.py
+++ train.py
@@ -7,7 +7,7 @@
 # neptune.log_metric('accuracy', 0.81)
 # neptune.log_metric('f1_score', 0.66)
 
-weird_text="trasdasdasda"
+weird_text="234234234"
 
 import os
 os.environ["NEPTUNE_EXPERIMENT_ID"] = "GIT-6" #neptune.get_experiment().id
