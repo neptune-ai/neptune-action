6e35c3b3c8217c21a77d44df4aca5a2eb6a5770d
jakubczakon <czakon.jakub@gmail.com>
2020-08-03 10:01:33 +0200

    ccc

--- train.py
+++ train.py
@@ -6,6 +6,6 @@ neptune.create_experiment(params={'lr': 0.3, 'epoch_nr': 40})
 
 neptune.log_metric('accuracy', 0.81)
 neptune.log_metric('f1_score', 0.66)
-
+neptune.close()
 # import os
 # os.environ["NEPTUNE_EXPERIMENT_ID"] = neptune.get_experiment().id
