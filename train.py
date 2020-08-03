f5ecb9aa933c00cf7a0d405cf0d1ecb302a17728
jakubczakon <czakon.jakub@gmail.com>
2020-08-03 15:17:08 +0200

    tried larged forest

--- train.py
+++ train.py
@@ -9,9 +9,9 @@ from sklearn.model_selection import train_test_split
 PROJECT_NAME = os.getenv('NEPTUNE_PROJECT_NAME')
 API_TOKEN = os.getenv('NEPTUNE_API_TOKEN')
 
-PARAMS = {'n_estimators': 10,
+PARAMS = {'n_estimators': 100,
           'max_depth': 30,
-          'max_features': 3,
+          'max_features': 5,
           }
 
 neptune.init(project_qualified_name=PROJECT_NAME, api_token=API_TOKEN)
