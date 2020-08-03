6ef49349df7a61a9de2d5cfc852dde0e27c8e57f
jakubczakon <czakon.jakub@gmail.com>
2020-08-03 13:49:25 +0200

    added tagging ci experiments

--- train.py
+++ train.py
@@ -1,12 +1,18 @@
+import os
+
 import neptune
 
-neptune.init(project_qualified_name='shared/github-actions', api_token='ANONYMOUS')
+PROJECT_NAME = os.getenv('NEPTUNE_PROJECT_NAME')
+API_TOKEN = os.getenv('NEPTUNE_API_TOKEN')
+
+neptune.init(project_qualified_name=PROJECT_NAME, api_token=API_TOKEN)
 
 neptune.create_experiment(params={'lr': 0.3, 'epoch_nr': 40})
 
 neptune.log_metric('accuracy', 0.81)
 neptune.log_metric('f1_score', 0.66)
 
-
-with open('experiment_id.txt', 'w+') as f:
-    f.write(neptune.get_experiment().id)
+if os.getenv('CI_PIPELINE') == "true":
+    neptune.append_tag('ci-pipeline')
+    with open('experiment_id.txt', 'w+') as f:
+        f.write(neptune.get_experiment().id)
