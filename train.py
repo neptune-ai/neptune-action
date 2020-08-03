be6257f59b081ef84a842010725bedcde08b52ca
jakubczakon <czakon.jakub@gmail.com>
2020-08-03 09:47:38 +0200

    changed smth in train

--- train.py
+++ train.py
@@ -2,9 +2,9 @@ import neptune
 
 neptune.init(project_qualified_name='shared/github-actions', api_token='ANONYMOUS')
 
-neptune.create_experiment(params={'lr': 0.3, 'epoch_nr': 40})
+neptune.create_experiment(params={'lr': 0.3, 'epoch_nr': 100})
 
-neptune.log_metric('accuracy', 0.81)
+neptune.log_metric('accuracy', 0.85)
 neptune.log_metric('f1_score', 0.66)
 
 import os
