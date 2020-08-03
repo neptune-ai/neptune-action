161ca66c28e36ded9ce259bdf6745d29420907d0
jakubczakon <czakon.jakub@gmail.com>
2020-08-03 09:50:39 +0200

    bl

--- train.py
+++ train.py
@@ -2,9 +2,9 @@ import neptune
 
 neptune.init(project_qualified_name='shared/github-actions', api_token='ANONYMOUS')
 
-neptune.create_experiment(params={'lr': 0.3, 'epoch_nr': 100})
+neptune.create_experiment(params={'lr': 0.3, 'epoch_nr': 66})
 
-neptune.log_metric('accuracy', 0.85)
+neptune.log_metric('accuracy', 0.99)
 neptune.log_metric('f1_score', 0.66)
 
 import os
