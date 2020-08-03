14569678ca0e17a96755c6a94415b9e2b1e07c24
Jakub <czakon.jakub@gmail.com>
2020-08-03 15:33:46 +0200

    Update train.py

--- train.py
+++ train.py
@@ -9,9 +9,9 @@ from sklearn.model_selection import train_test_split
 PROJECT_NAME = os.getenv('NEPTUNE_PROJECT_NAME')
 API_TOKEN = os.getenv('NEPTUNE_API_TOKEN')
 
-PARAMS = {'n_estimators': 10,
+PARAMS = {'n_estimators': 150,
           'max_depth': 30,
-          'max_features': 3,
+          'max_features': 6,
           }
 
 neptune.init(project_qualified_name=PROJECT_NAME, api_token=API_TOKEN)
